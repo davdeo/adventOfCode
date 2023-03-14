25b889a55773c80a76578eb1ada185e1299ffbda
david.monz <david.monz@macio.de>
2023-03-14 07:47:55 +0100

    continue level9

--- src/level9.ts
+++ src/level9.ts
@@ -1,5 +1,7 @@
+import { dir } from "console";
 import _, { last } from "lodash";
 import { readFile } from "./utils";
+import { writeFile } from "./utils/write";
 
 const VISITED_MARKER = '#';
 
@@ -7,7 +9,7 @@ enum Direction {
     U = 'U',
     UR = 'UR',
     R = 'R',
-    DR = 'UR',
+    DR = 'DR',
     D = 'D',
     DL = 'DL',
     L = 'L',
@@ -23,7 +25,7 @@ interface Position {
 class Map {
     private readonly EMPTY_FIELD = '.'
     private readonly map: string[][] = [[this.EMPTY_FIELD]];
-    private origin: Position = { x: 0, y:0 };
+    private origin: Position = { x: 0, y: 0 };
 
     private get height() {
         return this.map.length;
@@ -34,35 +36,35 @@ class Map {
     }
 
     private getMappedPosition(position: Position) {
-        return { x: this.origin.x + position.x, y: this.origin.y - position.y};
+        return { x: this.origin.x + position.x, y: this.origin.y - position.y };
     }
 
     private expandMap(direction: Direction, amount: number) {
-        switch(direction) {
+        switch (direction) {
             case Direction.U: {
-                for(let i = 0; i < amount; i++) {
+                for (let i = 0; i < amount; i++) {
                     this.map.unshift(this.EMPTY_FIELD.repeat(this.width).split(''))
                 }
                 this.origin.y += amount;
                 break;
             }
             case Direction.R: {
-                for(let i = 0; i < this.height; i++) {
-                    for(let j = 0; j < amount; j++) {
+                for (let i = 0; i < this.height; i++) {
+                    for (let j = 0; j < amount; j++) {
                         this.map[i].push(this.EMPTY_FIELD);
                     }
                 }
                 break;
             }
             case Direction.D: {
-                for(let i = 0; i < amount; i++) {
+                for (let i = 0; i < amount; i++) {
                     this.map.push(this.EMPTY_FIELD.repeat(this.width).split(''))
                 }
                 break;
             }
             case Direction.L: {
-                for(let i = 0; i < this.height; i++) {
-                    for(let j = 0; j < amount; j++) {
+                for (let i = 0; i < this.height; i++) {
+                    for (let j = 0; j < amount; j++) {
                         this.map[i].unshift(this.EMPTY_FIELD);
                     }
                 }
@@ -78,12 +80,12 @@ class Map {
             this.expandMap(Direction.D, Math.abs(this.height - 1 - this.origin.y + position.y));
         } else if (mappedPosition.y < 0) {
             this.expandMap(Direction.U, Math.abs(this.origin.y - position.y));
-        } 
+        }
 
-        mappedPosition = this.getMappedPosition(position);        
+        mappedPosition = this.getMappedPosition(position);
         if (mappedPosition.x < 0) {
             this.expandMap(Direction.L, - this.origin.x - position.x);
-        } else if (mappedPosition.x >= this.width) { 
+        } else if (mappedPosition.x >= this.width) {
             this.expandMap(Direction.R, this.origin.x - this.width + position.x + 1);
         }
 
@@ -109,6 +111,31 @@ class Map {
             process.stdout.write(`|\n`);
         })
         process.stdout.write(`x${'-'.repeat(this.width * 2 + 1)}x\n`)
+    }
+
+    serialize(): string {
+        let output = '';
+        output += `x${'-'.repeat(this.width * 2 + 1)}x\n`;
+        this.map.forEach((row, rowIndex) => {
+            output += `| `;
+            row.forEach((field, colIndex) => {
+                if (rowIndex === this.origin.y && colIndex === this.origin.x) {
+                    output += `o `;
+                } else {
+                    output += `${field} `;
+                }
+            })
+            output += `|\n`;
+        })
+        output += `x${'-'.repeat(this.width * 2 + 1)}x\n`;
+
+        return output;
+    }
+
+    clear() {
+        this.map.forEach((row, rowIndex) => row.forEach((position, colIndex) => {
+            this.map[rowIndex][colIndex] = this.EMPTY_FIELD;
+        }));
     }
 
     findVisitedPositions(marker: string) {
@@ -150,9 +177,9 @@ class Rope {
     private readonly startPosition: Position;
 
     constructor(length: number) {
-        this.startPosition = {x: 0, y: 0};
+        this.startPosition = { x: 0, y: 0 };
         this.knots = [];
-        for(let i=0; i<length; i++) {
+        for (let i = 0; i < length; i++) {
             this.knots.push(_.cloneDeep(this.startPosition))
         }
     }
@@ -169,10 +196,10 @@ class Rope {
         return this.knots.length;
     }
 
-    private moveKnot(knotIndex: number, direction: Direction) {
+    private moveKnot(knotIndex: number, direction: Direction, map: Map) {
         const knot = this.knots[knotIndex];
-        
-        switch(direction) {
+
+        switch (direction) {
             case Direction.U: {
                 knot.y += 1;
                 break;
@@ -212,69 +239,77 @@ class Rope {
         }
     }
 
-    moveHead(direction: Direction) {
-        this.moveKnot(0, direction);        
+    moveHead(direction: Direction, map: Map) {
+        this.moveKnot(0, direction, map);
 
         if (this.length <= 1) {
             return;
-        } 
+        }
 
-        const head = this.head;
-        const second = this.knots[1];
-        const subsequentDirection = getDirection(second, head);
-        const distance = this.measureDistance(head, second);
-
-        this.moveSubsequentKnot(1, distance >= 2 ? subsequentDirection : Direction.NONE);
+        this.moveSubsequentKnot(1, map);
+        // map.visit(this.head, 'H');
     }
 
-    private moveSubsequentKnot(knotIndex: number, direction: Direction){
-        if (knotIndex === this.length) {
+    private moveSubsequentKnot(knotIndex: number, map: Map) {
+        const fronKnot = this.knots[knotIndex - 1];
+        const currentKnot = this.knots[knotIndex];
+        const distance = this.measureDistance(fronKnot, currentKnot);
+        const direction = distance >= 2 ? getDirection(currentKnot, fronKnot) : Direction.NONE;
+
+        if (_.isEqual(this.knots[knotIndex], this.startPosition)) {
+            this.moveKnot(knotIndex, direction, map);
+            // map.visit(this.knots[knotIndex], '#');
+
             return;
         }
 
-        if (knotIndex > this.knots.findIndex((knot) => knot.x === this.startPosition.x && knot.y === this.startPosition.y)) {
+        this.moveKnot(knotIndex, direction, map);
+        // map.visit(this.knots[knotIndex], '#');
+
+
+        if (knotIndex === this.length - 1) {
             return;
         }
-        
-        this.moveKnot(knotIndex, direction);
-        
-        this.moveSubsequentKnot(knotIndex + 1, direction);
+
+        this.moveSubsequentKnot(knotIndex + 1, map);
     }
 
     private measureDistance(pos1: Position, pos2: Position) {
         const diffX = pos1.x - pos2.x;
         const diffY = pos1.y - pos2.y;
-    
+
         return Math.sqrt(diffX * diffX + diffY * diffY);
     }
 }
 
 function run(moves: Move[], map: Map) {
-    const rope = new Rope(6);
+    const rope = new Rope(10);
 
     moves.forEach((move) => {
-        for(let i=0; i<move.amount; i++) {
-            rope.moveHead(move.direction)
-            map.visit(rope.head, 'h');
-            map.visit(rope.tail, VISITED_MARKER);
+        for (let i = 0; i < move.amount; i++) {
+            rope.moveHead(move.direction, map);
+            map.visit(rope.tail, '#');
+            // map.print();
         }
     })
 }
 
 export function run9() {
-    // const input = readFile('input/level9/level9.in');
-    const input = readFile('input/level9/example.in');
-    
+    const input = readFile('input/level9/level9.in');
+    // const input = readFile('input/level9/example.in');
+
     const moves = input.map((line): Move => {
         const moveMeta = line.split(" ");
-        return {direction: moveMeta[0] as Direction, amount: parseInt(moveMeta[1])}
+        return { direction: moveMeta[0] as Direction, amount: parseInt(moveMeta[1]) }
     })
 
     const map = new Map();
     map.print();
-    
+
     run(moves, map);
-    map.print();
-    
+    // map.print();
+
+    writeFile('output/level9/level9.out', map.serialize());
+
     return map.findVisitedPositions(VISITED_MARKER);
 }
---
+++ src/utils/write.ts
@@ -0,0 +1,6 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function writeFile(filePath: string, data: string): void {
+    const file = fs.writeFileSync(path.join(__dirname, '../', filePath), data, { flag: 'a' });
+}
