be677115543c5d5754c6639cc3477cc36cb0e1a2
david.monz <david.monz@macio.de>
2023-03-26 13:54:06 +0200

    start level 10

---
+++ src/level10.ts
@@ -0,0 +1,10 @@
+import _ from "lodash";
+import { readFile } from "./utils";
+import { writeFile } from "./utils";
+
+export function run10() {
+    const input = readFile('input/level10/level10.in');
+    // const input = readFile('input/level10/example.in');
+
+    return 0;
+}
