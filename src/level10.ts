c85d2fdb90e8d5744c27d9635762a9cf7f27d312
David <david.monz@hotmail.com>
2023-04-07 09:15:43 +0200

    finish level 10

--- src/level10.ts
+++ src/level10.ts
@@ -2,14 +2,19 @@ import _ from "lodash";
 import { readFile } from "./utils";
 import { writeFile } from "./utils";
 
-enum CommandType {
+enum OperationType {
     ADD_X = 'addx',
     NOOP = 'noop'
 }
 
 interface Command {
-    type: CommandType;
+    type: OperationType;
     amount: number;
+}
+
+interface Operation {
+    type: OperationType;
+    amount?: number;
 }
 
 function getCommands(input: string[]): Command[] {
@@ -17,7 +22,7 @@ function getCommands(input: string[]): Command[] {
         const lineMeta = line.split(' ');
 
         return {
-            type: lineMeta[0] as CommandType,
+            type: lineMeta[0] as OperationType,
             amount: parseInt(lineMeta[1] ?? 0)
         }
     });
@@ -26,36 +31,51 @@ function getCommands(input: string[]): Command[] {
 class Machine {
     private readonly MONITORING_INTERVAL = 40;
     private readonly FIRST_MONITORING = 20;
+    private readonly CRT_LINE_LENGTH = 40;
+    private readonly CRT_LIGHT_PIXEL = '#';
+    private readonly CRT_DARK_PIXEL = ' '
     private x: number = 1;
     private cycle: number = 0;
     private summedSignalStrengths = 0;
+
+    private commandStack: Command[] = [];
+    private operationStack: Operation[] = [];
+    private crt: string[] = [];
 
     private addX(amount: number) {
         this.x += amount;
     }
 
-    private executeAddX(command: Command) {
-        this.executeNoop();
-        this.executeNoop();
+    private executeNextOperation() {
+        const nextOperation = this.operationStack.pop();
 
-        this.addX(command.amount);
-    }
-
-    private executeNoop() {
-        this.cycle ++;
-        this.monitor();
-    }
-
-    private executeCommand(command: Command) {
-        switch(command.type) {
-            case CommandType.ADD_X: {
-                this.executeAddX(command);
+        switch (nextOperation?.type) {
+            case OperationType.ADD_X: {
+                this.addX(nextOperation?.amount ?? 0);
                 return;
             }
-            case CommandType.NOOP: {
-                this.executeNoop();
+            case OperationType.NOOP: {
                 return;
             }
+            default:
+                return;
+        }
+    }
+
+
+    private addNextCommand(command?: Command) {
+        switch (command?.type) {
+            case OperationType.ADD_X: {
+                this.operationStack.push({ type: OperationType.ADD_X, amount: command.amount });
+                this.operationStack.push({ type: OperationType.NOOP });
+                return;
+            }
+            case OperationType.NOOP: {
+                this.operationStack.push({ type: OperationType.NOOP });
+                return;
+            }
+            default:
+                return;
         }
     }
 
@@ -66,10 +86,59 @@ class Machine {
         }
     }
 
-    run(commands: Command[]) {
-        commands.forEach((command) => {
-            this.executeCommand(command);
+    private writeCrt() {
+        const spriteLowerBorder = this.x - 1;
+        const spriteUpperBorder = this.x + 1;
+        const position = this.cycle % this.CRT_LINE_LENGTH - 1;
+
+        if (position >= spriteLowerBorder && position <= spriteUpperBorder) {
+            this.crt.push(this.CRT_LIGHT_PIXEL);
+        } else {
+            this.crt.push(this.CRT_DARK_PIXEL);
+        }
+    }
+
+    draw() {
+        this.crt.forEach((pixel, index) => {
+            if (index % this.CRT_LINE_LENGTH === 0) {
+                process.stdout.write('\n');
+            }
+
+            process.stdout.write(pixel);
         })
+
+        process.stdout.write('\n');
+    }
+
+    init(commands: Command[]) {
+        this.commandStack.push(...commands.reverse());
+    }
+
+    private executeCycle() {
+        if (this.operationStack.length <= 0 && this.commandStack.length > 0) {
+            const nextCommand = this.commandStack.pop();
+
+            this.addNextCommand(nextCommand);
+        }
+
+        if (this.operationStack.length <= 0) {
+            return;
+        }
+
+        this.cycle++;
+
+        // this.monitor();
+        this.writeCrt();
+
+        this.executeNextOperation();
+    }
+
+    execute() {
+        while (this.commandStack.length > 0 || this.operationStack.length > 0) {
+            this.executeCycle();
+        }
+
+        console.log("end of operation")
 
         return this.summedSignalStrengths;
     }
@@ -82,7 +151,9 @@ export function run10() {
     const commands = getCommands(input);
 
     const machine = new Machine();
-    const summedSignalStrengths = machine.run(commands);
+    machine.init(commands);
+    machine.execute();
+    machine.draw();
 
-    return summedSignalStrengths;
+    return 0;
 }
