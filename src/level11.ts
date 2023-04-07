cddc4303e8d69ee726b1299e9e1adc55ad76de91
David <david.monz@hotmail.com>
2023-04-07 09:15:57 +0200

    prepare for level 11

---
+++ src/level11.ts
@@ -0,0 +1,11 @@
+import _ from "lodash";
+import { readFile } from "./utils";
+import { writeFile } from "./utils";
+
+export function run11() {
+    // const input = readFile('input/level11/level11.in');
+    const input = readFile('input/level11/example.in');
+
+
+    return 0;
+}
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@
 // import { run7 } from "./level7";
 // import { run8 } from "./level8";
 // import { run9 } from "./level9";
-import { run10 } from "./level10";
+// import { run10 } from "./level10";
+import { run11 } from "./level11";
 
 // console.log(run1());
 // console.log(run2())
@@ -17,4 +18,5 @@ import { run10 } from "./level10";
 // console.log(run6())
 // console.log(run7())
 // console.log(run9())
-console.log(run10())
+// console.log(run10())
+console.log(run11())
