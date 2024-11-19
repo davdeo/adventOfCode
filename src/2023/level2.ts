ad272fc117217d7eb87636b096bd45e505622d60
davidm <david.monz@hotmail.com>
2024-11-19 20:47:54 +0100

    finish level 2

--- src/2023/level2.ts
+++ src/2023/level2.ts
@@ -1,3 +1,4 @@
+import { forIn } from "lodash";
 import {Input, readFile, writeFile} from "../utils";
 
 export function runlevel2() {
@@ -5,17 +6,99 @@ export function runlevel2() {
     console.log("Run level2");
 
     const exampleInput = readFile('2023/input/level2/example.in');
-    const exampleResult = calculateResult(exampleInput);
+    const exampleResult = calculateResult2(exampleInput);
     console.log("exampleOutput:", exampleResult);
 
     const input = readFile('2023/input/level2/level2.in');
-    const result = calculateResult(input);
+    const result = calculateResult2(input);
     writeFile('2023/output/level2.out', `${result}`)
 }
 
 function calculateResult(input: Input) {
     const {lines} = input;
-    const res = 0;
+    let res = 0;
+
+    const greenMaxCount = 13;
+    const blueMaxCount = 14;
+    const redMaxCount = 12; 
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        console.log();
+        console.log(line);
+        let isPossible = true;
+
+        for (const m of line.matchAll(/\d+ blue/g)) {
+            const num = parseInt(m[0].split(" ")[0])
+            
+            if (num > blueMaxCount) {
+                isPossible = false;
+            }
+        }
+
+        for (const m of line.matchAll(/\d+ red/g)) {
+            const num = parseInt(m[0].split(" ")[0])
+             
+            if (num > redMaxCount) {
+                isPossible = false;
+            }
+        }
+
+        for (const m of line.matchAll(/\d+ green/g)) {
+            const num = parseInt(m[0].split(" ")[0])
+            
+            if (num > greenMaxCount) {
+                isPossible = false;
+            }
+        }
+
+        console.log(isPossible)
+        if (isPossible) {
+            res += i + 1;
+        }
+    }
+
+    return res;
+}
+
+
+function calculateResult2(input: Input) {
+    const {lines} = input;
+    let res = 0;
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        let minGreen = 0;
+        let minRed = 0;
+        let minBlue = 0;
+
+        for (const m of line.matchAll(/\d+ blue/g)) {
+            const num = parseInt(m[0].split(" ")[0])
+            
+            if (num > minBlue) {
+                minBlue = num;
+            }
+        }
+        
+        for (const m of line.matchAll(/\d+ red/g)) {
+            const num = parseInt(m[0].split(" ")[0])
+            
+            if (num > minRed) {
+                minRed = num;
+            }
+        }
+        
+        for (const m of line.matchAll(/\d+ green/g)) {
+            const num = parseInt(m[0].split(" ")[0])
+            
+            if (num > minGreen) {
+                minGreen = num;
+            }
+        }
+        
+        const power = minGreen * minBlue * minRed;
+        res += power;
+    }
 
     return res;
 }
