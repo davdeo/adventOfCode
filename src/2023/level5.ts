b62565645cb91ca0a1a8d4921d8502ba102dd434
davidmonz <david.monz@cloudflight.io>
2024-11-27 18:17:42 +0100

    finish level 5

--- src/2023/level5.ts
+++ src/2023/level5.ts
@@ -1,5 +1,5 @@
 import {Input, readFile, writeFile} from "../utils";
-import {keys} from "lodash";
+import {isEmpty, keys} from "lodash";
 
 export function runlevel5() {
     console.log();
@@ -9,9 +9,9 @@ export function runlevel5() {
     const exampleResult = calculateResult1(exampleInput);
     console.log("exampleOutput:", exampleResult);
 
-    // const input = readFile('2023/input/level5/real.in');
-    // const result = calculateResult1(input);
-    // writeFile('2023/output/level5.out', `${result}`)
+    const input = readFile('2023/input/level5/real.in');
+    const result = calculateResult1(input);
+    writeFile('2023/output/level5.out', `${result}`)
 }
 
 interface Map {
@@ -20,7 +20,11 @@ interface Map {
     range: number;
 }
 
-function getTarget(n: number, maps: Map[]): number {
+interface GroupOfMaps {
+    maps: Map[];
+}
+
+function getSingleTarget(n: number, maps: Map[]): number {
     for (const map of maps) {
         const {targetIndex, sourceIndex, range} = map;
         if (n >= sourceIndex && n < sourceIndex + range) {
@@ -31,12 +35,66 @@ function getTarget(n: number, maps: Map[]): number {
     return n;
 }
 
-function calculateResult1(input: Input) {
-    const {lines} = input;
-    let res = 0;
+function getTarget(n: number, allMaps: GroupOfMaps[]): number {
+    let currentResult = n;
+    allMaps.forEach(groupOfMaps => {
+        currentResult = getSingleTarget(currentResult, groupOfMaps.maps)
+    })
 
-    const maps: Map[] = [];
-    console.log(1, getTarget(1, maps))
+    return currentResult;
+}
+
+function calculateResult1(input: Input) {
+    let {lines} = input;
+    let res = Number.MAX_SAFE_INTEGER;
+
+    const allMaps: GroupOfMaps[] = [];
+    allMaps.push({maps: []})
+
+    let index = 0;
+
+    lines.reverse().forEach((line) => {
+        if (isEmpty(line)) {
+            return;
+        }
+
+        if (line.match(/seeds:/)) {
+            return;
+        }
+
+        if (line.match(/:/)) {
+            index ++;
+            allMaps.push({maps: []});
+            return;
+        }
+
+        const map = line.split(" ").map(v => parseInt(v.trim()));
+        allMaps[index].maps.push({
+            targetIndex: map[0],
+            sourceIndex: map[1],
+            range: map[2]
+        })
+
+    })
+
+    allMaps.pop();
+    allMaps.reverse();
+
+    const seedsLine = lines[lines.length - 1].split(" ");
+    seedsLine.shift();
+    const seedsLineNum = seedsLine.map(v => parseInt(v.trim()));
+    for(let i = 0; i < seedsLineNum.length; i = i+2) {
+        const seed = seedsLineNum[i];
+        const numberOfSeeds = seedsLineNum[i+1];
+        console.log(seed);
+        console.log(numberOfSeeds);
+        for (let j = 0; j < numberOfSeeds; j++) {
+            const location = getTarget(seed + j, allMaps);
+            if (location < res) {
+                res = location;
+            }
+        }
+    }
 
     return res;
 }
